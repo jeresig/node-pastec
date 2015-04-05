3bf984e3e3754d3b988702c7056e4f1a32da8bda
John Resig <jeresig@gmail.com>
2015-04-04 20:21:10 -0400

    Initial version of Pastec module.

---
+++ pastec.js
@@ -0,0 +1,70 @@
+var fs = require("fs");
+var path = require("path");
+
+var request = require("request");
+
+module.exports = function(conf) {
+    conf.server = conf.server || "localhost:4212";
+
+    return {
+        url: "http://" + conf.server + "/index/",
+
+        handle: function(callback, expected) {
+            return function(err, res, body) {
+                if (!callback) {
+                    return;
+                }
+
+                if (err || res.statusCode !== 200) {
+                    return callback(err);
+                }
+
+                var data = JSON.parse(body);
+
+                if (expected && data.type !== expected) {
+                    return callback(data);
+                }
+
+                callback(null, data.result || data);
+            };
+        },
+
+        del: function(id, callback) {
+            request.del(this.url + "images/" + id,
+                 this.handle(callback, "IMAGE_REMOVED"));
+        },
+
+        list: function(callback) {
+            // NOTE: Need implementation in Pastec
+        },
+
+        similar: function(file, callback) {
+            // NOTE: Need implementation in Pastec
+        },
+
+        fileSimilar: function(file, callback) {
+            fs.createReadStream(file)
+                .pipe(request.post(this.url + "searcher",
+                    this.handle(callback, "SEARCH_RESULTS")));
+        },
+
+        urlSimilar: function(url, callback) {
+            request.get(url)
+                .pipe(request.post(this.url + "searcher",
+                    this.handle(callback, "SEARCH_RESULTS")));
+        },
+
+        add: function(files, dir, callback) {
+            if (!(files instanceof Array)) {
+                files = [files];
+            }
+
+            async.mapLimit(files, 1, function(file, callback) {
+                var id = dir + "/" + path.basename(file);
+                fs.createReadStream(file)
+                    .pipe(request.put(this.url + "images/" + id,
+                        this.handle(callback, "IMAGE_ADDED")));
+            }.bind(this), callback);
+        }
+    };
+};
